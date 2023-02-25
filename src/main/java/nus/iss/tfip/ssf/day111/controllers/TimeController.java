6c6d77f4187b18b9ae088df06afd5917f81b9502
chuahce <choongeng.chuah@gmail.com>
2023-02-25 23:11:51 +0800

    SSF Assessment Revision

---
+++ src/main/java/nus/iss/tfip/ssf/day111/controllers/TimeController.java
@@ -0,0 +1,32 @@
+package nus.iss.tfip.ssf.day111.controllers;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// Tell Spring Boot this is the controller
+@Controller
+@RequestMapping(path={"/time.html", "/time"})
+public class TimeController {
+
+  // GET /time
+  @GetMapping
+  public String getTime(Model model) {
+
+    Date date = new Date();
+    String currentTime = date.toString();
+
+    System.out.printf(">>> the current time is %s\n", currentTime);
+
+    // Add the time to model
+    model.addAttribute("time", currentTime);
+
+    // return the view
+    return "time";
+  } 
+}
+
+
